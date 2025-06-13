d11bfda3b9196057adae593b43d090a96d629752
Karan Sahu <sahukaran298@gmail.com>
2025-06-14 00:41:11 +0530

    paddles file including paddles behaviour

---
+++ Day 22 Project/paddles.py
@@ -0,0 +1,22 @@
+from turtle import Turtle, Screen
+screen = Screen()
+
+#paddle class
+class Paddle(Turtle):
+    def __init__(self, x_position):
+        super().__init__()
+        self.shape("square")
+        self.color("white")
+        self.shapesize(stretch_wid=5, stretch_len=1)
+        self.penup()
+        self.goto(x_position, 0)
+
+    def go_up(self):
+        new_y = self.ycor() + 20
+        self.sety(new_y)
+
+    def go_down(self):
+        new_y = self.ycor() - 20
+        self.sety(new_y)
+    
+    
